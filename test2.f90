0ceb83f43cdfaa777423c30db0906ace4d6432e8
olovink <69630128+olovink@users.noreply.github.com>
2023-07-16 00:10:00 +0700

    Update test2.f90

--- test2.f90
+++ test2.f90
@@ -2,5 +2,5 @@ program test_cpu_time
     real :: start, finish
     call cpu_time(start)
     call cpu_time(finish)
-    print '("Time = ",f6.3," seconds.")',finish-start
+    print *, '("Time = ",f6.3," seconds.")' , finish-start
 end program test_cpu_time
