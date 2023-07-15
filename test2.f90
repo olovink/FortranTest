456c526d8100b2275f12aa028f995741b76e44f7
olovink <69630128+olovink@users.noreply.github.com>
2023-07-16 00:09:26 +0700

    Create test2.f90

---
+++ test2.f90
@@ -0,0 +1,6 @@
+program test_cpu_time
+    real :: start, finish
+    call cpu_time(start)
+    call cpu_time(finish)
+    print '("Time = ",f6.3," seconds.")',finish-start
+end program test_cpu_time
